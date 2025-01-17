ea98431d23dcb26f2bdc85923a8ee71bb7814390
UndeswardLicht <AliaxejBusleikaV+github@gmail.com>
2025-01-17 23:44:59 +0100

    changed DB schema,changed OLTP script,  added work with items in load script

--- oltp/create tables.sql
+++ oltp/create tables.sql
@@ -1,11 +1,12 @@
+drop table if exists Latest_Updates;
+drop table if exists Items_Purchases;
+drop table if exists Items_Rents;
 drop table if exists Rents;
 drop table if exists Purchases;
-drop table if exists Items_Rents;
 drop table if exists Items;
-drop table if exists Payments;
 drop table if exists Rentees;
-drop table if exists Subcategories;
 drop table if exists Staff;
+drop table if exists Subcategories;
 drop table if exists Categories;
 
 CREATE TABLE Categories(
@@ -13,28 +14,22 @@ CREATE TABLE Categories(
     category_name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Staff(
-    id SERIAL PRIMARY KEY,
-    first_name INT NOT NULL,
-    last_name INT NOT NULL
-);
-
 CREATE TABLE Subcategories(
     id SERIAL PRIMARY KEY,
     subcategory_name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Staff(
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE Rentees(
     passport_id VARCHAR(255) NOT NULL PRIMARY KEY UNIQUE,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE Payments(
-    id SERIAL PRIMARY KEY,
-    amount MONEY NOT NULL,
-    payment_date TIME(0) WITHOUT TIME ZONE NOT NULL
+    phone VARCHAR(255) 
 );
 
 CREATE TABLE Items(
@@ -43,36 +38,59 @@ CREATE TABLE Items(
     brand VARCHAR(255) NOT NULL,
     category_id INT NOT NULL,
     subcategory_id INT NOT NULL,
-    sold BOOLEAN NOT NULL DEFAULT FALSE,
+    stock_quantity INT NOT NULL DEFAULT 1,
+    price DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
+    rent_price DECIMAL(10, 2) NOT NULL DEFAULT 0.00,
     FOREIGN KEY(subcategory_id) REFERENCES Subcategories(id),
-    FOREIGN KEY(category_id) REFERENCES Categories(id)
+    FOREIGN KEY(category_id) REFERENCES Categories(id),
+    CONSTRAINT stock_nonnegative CHECK (stock_quantity >= 0),
+    CONSTRAINT price_nonnegative CHECK (price >= 0),
+    CONSTRAINT rent_price_nonnegative CHECK (rent_price >= 0)
+);
+
+
+CREATE TABLE Purchases(
+    id SERIAL PRIMARY KEY,
+    staff_member_id INT NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL,
+    payment_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    FOREIGN KEY(staff_member_id) REFERENCES Staff(id)
+);
+
+CREATE TABLE Rents(
+    id SERIAL PRIMARY KEY,
+    rentee_id VARCHAR(255) NOT NULL,
+    rented_from DATE NOT NULL,
+    rented_to DATE NOT NULL,
+    payment_date TIMESTAMP WITHOUT TIME ZONE NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY(rentee_id) REFERENCES Rentees(passport_id)
 );
 
 CREATE TABLE Items_Rents(
     item_id INT NOT NULL UNIQUE,
     rent_id INT NOT NULL UNIQUE,
+    quantity INT NOT NULL DEFAULT 1,
     FOREIGN KEY(item_id) REFERENCES Items(id),
-    PRIMARY KEY(item_id, rent_id)
+    FOREIGN KEY(rent_id) REFERENCES Rents(id),
+    PRIMARY KEY(item_id, rent_id),
+    CONSTRAINT quantity_nonnegative CHECK (quantity >= 0)
 );
 
-CREATE TABLE Purchases(
-    id SERIAL PRIMARY KEY,
-    item_id INT NOT NULL,
-    payment_id INT NOT NULL,
-    staff_member_id INT NOT NULL,
+CREATE TABLE Items_Purchases(
+    item_id INT NOT NULL UNIQUE,
+    purchase_id INT NOT NULL UNIQUE,
+    quantity INT NOT NULL DEFAULT 1,
     FOREIGN KEY(item_id) REFERENCES Items(id),
-    FOREIGN KEY(staff_member_id) REFERENCES Staff(id),
-    FOREIGN KEY(payment_id) REFERENCES Payments(id)
+    FOREIGN KEY(purchase_id) REFERENCES Purchases(id),
+    PRIMARY KEY(item_id, purchase_id),
+    CONSTRAINT quantity_nonnegative CHECK (quantity >= 0)
 );
 
-CREATE TABLE Rents(
-    id SERIAL PRIMARY KEY,
-    item_id INT NOT NULL,
-    payment_id INT NOT NULL,
-    rentee_id VARCHAR(255) NOT NULL,
-    rented_from TIME(0) WITHOUT TIME ZONE NOT NULL,
-    rented_to TIME(0) WITHOUT TIME ZONE NOT NULL,
-    FOREIGN KEY (payment_id) REFERENCES Payments(id),
-    FOREIGN KEY(item_id) REFERENCES Items_Rents(rent_id),
-    FOREIGN KEY(rentee_id) REFERENCES Rentees(passport_id)
+CREATE TABLE Latest_Updates(
+	id SERIAL PRIMARY KEY,
+    latest_update_purchase TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT '2001-01-01 01:01:01',
+    latest_update_rent TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT '2001-01-01 01:01:01',
+    latest_update_item TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT '2001-01-01 01:01:01'
 );
+
--- oltp/load data script.sql
+++ oltp/load data script.sql
@@ -31,14 +31,16 @@ drop table if exists temp_subcategories;
 CREATE TEMP TABLE temp_items (
     item_full_name VARHCAR(255),
     category VARHCAR(255),
-    subcategory VARHCAR(255)
+    subcategory VARHCAR(255),
+    price VARCHAR(255),
+    rent_price VARCHAR(255),
+    delivered DATE
 );
 
 CREATE TEMP TABLE temp_purchases(
     purchase_date DATE,
     employee_full_name VARHCAR(255),
-    item_full_name VARHCAR(255),
-    amount MONEY
+    item_full_name VARHCAR(255)
 );
 
 CREATE TEMP TABLE temp_rents(
@@ -46,8 +48,7 @@ CREATE TEMP TABLE temp_rents(
     from_date DATE,
     to_date DATE,
     when_paid DATE,
-    customer VARHCAR(255),
-    amount MONEY
+    customer VARHCAR(255)
 );
 
 CREATE TEMP TABLE temp_customers(
@@ -73,6 +74,129 @@ COPY temp_purchases from {file_name} WITH (FORMAT csv, HEADER true, DELIMITER ',
 COPY temp_rents from {file_name} WITH (FORMAT csv, HEADER true, DELIMITER ',', QUOTE '"');
 COPY temp_customers from {file_name} WITH (FORMAT csv, HEADER true, DELIMITER ',', QUOTE '"');
 COPY temp_staff from {file_name} WITH (FORMAT csv, HEADER true, DELIMITER ',', QUOTE '"');
+
+
+INSERT INTO categories(category_name)
+SELECT i.category
+FROM temp_items i
+WHERE NOT EXISTS(
+    SELECT 1 from categories
+    WHERE category_name = i.category
+);
+
+INSERT INTO subcategories(subcategory_name)
+SELECT i.subcategory
+FROM temp_items i
+WHERE NOT EXISTS(
+    SELECT 1 from subcategories
+    WHERE subcategory_name = i.subcategory
+);
+
+INSERT INTO rentees(passport_id, first_name, last_name, phone)
+SELECT (
+    c.passport,
+    split_part(c.full_name, ' ', 1) AS first_name,
+    split_part(c.full_name, ' ', 2) AS last_name,
+    c.phone
+) 
+FROM temp_customers c
+WHERE NOT EXISTS(
+    SELECT 1 from rentees
+    WHERE passport_id = c.passport
+);
+
+--loophole -> it's possible that two full namesakes will work there.
+--posible solution: add a field to distinguish two people - passport data or middle/father's name?
+INSERT INTO staff(first_name, last_name)
+SELECT (
+    split_part(s.full_name, ' ', 1) AS first_name,
+    split_part(s.full_name, ' ', 2) AS last_name
+)
+FROM temp_staff s
+WHERE NOT EXISTS(
+    SELECT 1 from staff
+    WHERE first_name = s.first_name AND last_name = s.last_name
+);
+
+
+--loophole -> there can be >1 items of identical models of the same brand
+--possible solution: add timestamp of 'shipping date', column 'number on stock' and a check to verify
+-- whether the item should be inserted once again or not 
+
+--this query select totaly new items
+WITH last_delivery_date AS
+(
+    SELECT latest_update_item from Latest_Updates
+),
+new_items AS
+(
+    SELECT item_full_name, category, subcategory, price, rent_price
+    FROM temp_items
+    WHERE delivered > last_delivery_date
+)
+
+INSERT INTO items(model, brand, category_id, subcategory_id, price, rent_price)
+SELECT (
+    split_part(ni.item_full_name, ' ', 1) AS brand,
+    split_part(ni.item_full_name, ' ', 2) AS model,
+    (SELECT id FROM categories WHERE category_name = ni.category),
+    (SELECT id FROM subcategories WHERE subcategory_name = ni.subcategory),
+    ni.price,
+    ni.rent_price
+)
+FROM new_items ni
+WHERE NOT EXISTS(
+    SELECT 1 FROM items
+    WHERE CONCAT(model, ' ', brand)) = ni.item_full_name 
+);
+
+--this query adds quantity if there were new already known items
+WITH last_delivery_date AS
+(
+    SELECT latest_update_item from Latest_Updates
+),
+newly_delivered_items AS
+(
+    SELECT item_full_name
+    FROM temp_items
+    WHERE delivered > last_delivery_date
+)
+UPDATE items
+SET stock_quantity = stock_quantity + 1
+WHERE CONCAT(model, ' ', brand) in newly_delivered_items;
+
+--query to beatify data in table
+UPDATE items
+SET model = UPPER(model),
+    category_name = INITCAP(category_name),
+    subcategory_name = INITCAP(subcategory_name)
+
+
+
+
+--loophole -> it's possible that there will be made more than one payment for the same amount.
+--posible solution: date format should be timestamp - yyyy-mm-dd-hh-mm-ss
+INSERT INTO payments(columns_name)
+SELECT *
+FROM temp_purchases;
+
+INSERT INTO payments(columns_name)
+SELECT * 
+FROM temp_rents
+
+
+INSERT INTO -purchases(columns_name)
+SELECT * from temp_purchases
+
+INSERT INTO rents(columns_name)
+SELECT * from temp_rents
+
+
+
+
+INSERT INTO items_rents(columns_name)
+SELECT * from temp_rents
+
 
 
 
